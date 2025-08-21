db09b5a95efdad0bd9722d78debc9d566f47d351
shimil.jas <shimil.jas@cashfree.com>
2025-08-21 15:47:42 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: './', // Use relative paths for assets
+  build: {
+    outDir: 'dist', // Netlify expects build output in dist
+  },
 })
